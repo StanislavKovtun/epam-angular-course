f074f38fdfa02ae96d65eb64623805efa5c90654
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2023-05-26 13:08:21 +0300

    Changed structure of files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LogoComponent } from './header/logo/logo.component';
-import { FooterComponent } from './footer/footer.component';
-import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { CoursesComponent } from './courses/courses.component';
     HeaderComponent,
     LogoComponent,
     FooterComponent,
-    CoursesComponent
+    CoursesComponent,
+    BreadcrumbsComponent,
+    CoursesPageComponent,
+    LoginComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
