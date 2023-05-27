f9de64052cf00ddd6c90587009fcf20c3bc5b734
Stanislav Kovtun <sstanisslaw@gmail.com>
2023-05-27 18:51:42 +0300

    Changed file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoComponent } from './components/logo/logo.component';
+import { LogoComponent } from './components/header/logo/logo.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CoursesComponent } from './components/courses/courses.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CoursesComponent } from './components/courses-page/courses/courses.component';
+import { BreadcrumbsComponent } from './components/courses-page/breadcrumbs/breadcrumbs.component';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 import { LoginComponent } from './components/login/login.component';
-import { SectionComponent } from './components/section/section.component';
+import { SectionComponent } from './components/courses-page/section/section.component';
 
 @NgModule({
   declarations: [
