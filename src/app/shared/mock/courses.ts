0a9785a12b9c47ddb19bc00969cf0ae964565979
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2023-05-29 14:58:47 +0300

    Changed some styles

--- src/app/shared/mock/courses.ts
+++ src/app/shared/mock/courses.ts
@@ -3,7 +3,7 @@ import { ICourse } from '../interfaces/course';
 export const courses: ICourse[] = [
   {
     id: 1,
-    title: 'Course 1',
+    title: 'HTML',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Learn about where you find course description, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college`s class.They`re published both in course catalog.',
     creationDate: '2023-05-18',
@@ -11,7 +11,7 @@ export const courses: ICourse[] = [
   },
   {
     id: 2,
-    title: 'Course 2',
+    title: 'CSS',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!',
       creationDate: '2023-05-20',
@@ -19,7 +19,7 @@ export const courses: ICourse[] = [
   },
   {
     id: 3,
-    title: 'Course 3',
+    title: 'JavaScript',
     description:
       'Test description. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!',
       creationDate: '2023-05-21',
